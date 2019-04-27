e1f839c01b77c3d330ad1363f85f637035cec466
avbravo <avbravo@avbravo>
2019-04-26 20:39:26 -0500

    primeraversion

---
+++ src/main/java/com/avbravo/practicaejb/entity/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.practicaejb.entity;
+
+import javax.persistence.Id;
+
+/**
+ *
+ * @author avbravo
+ */
+public class Persona {
+     @Id
+    private String cedula;
+
+    private String nombre;
+    private Integer edad;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+}
